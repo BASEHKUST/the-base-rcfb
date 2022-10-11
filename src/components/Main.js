c96f2426b55893bdedaf4ae9e37f2e7220976948
keaneipygg <Keane.W@ipygg.com>
2022-10-11 14:56:35 +0800

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+// Firebase Configuration DON'T TOUCH!
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+const firebaseConfig = {
+    apiKey: "AIzaSyDFK9tCGFyCU_yDjRMmg_hK1XyYaFJHH4Y",
+    authDomain: "the-base-rcfb-7159f.firebaseapp.com",
+    projectId: "the-base-rcfb-7159f",
+    storageBucket: "the-base-rcfb-7159f.appspot.com",
+    messagingSenderId: "67790081552",
+    appId: "1:67790081552:web:a1efaf4927e83494dcc495",
+    measurementId: "G-R9WQY7C2ER"
+};
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+class Main extends Component {
+    state = {}
+    render() {
+        return (
+            <React.Fragment>
+                <h1>The Base Website !</h1>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Main;
