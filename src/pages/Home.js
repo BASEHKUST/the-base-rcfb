a27754ee5dab15d6107c3054d009abb7e206012f
keaneipygg <Keane.W@ipygg.com>
2022-12-06 20:58:26 +0800

    implement footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import HeaderAndNavbar from '../components/HeaderAndNavbar';
 import HomeBlueBar from '../components/HomeBlueBar'
+import Footer from '../components/Footer'
 
 class Home extends Component {
     state = {  } 
@@ -10,6 +11,8 @@ class Home extends Component {
                 <HeaderAndNavbar />
                 <h1>HOME PAGE</h1>
                 <HomeBlueBar title = "Hello ! We are The BASE Entrepreneurship Ambassadors!" content = "We are a group of students at HKUST who aim to educate what entrepreneurship means and promote entrepreneurship culture whilst providing a dedicated coworking space"/> 
+                <div style={{ width: '100%', height: '500px' }}>PLACE HOLDER</div>
+                <Footer />
             </React.Fragment>
         );
     }
