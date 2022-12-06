51eeadcbb2101a43146cac5bdc0f17977d01ab69
keaneipygg <Keane.W@ipygg.com>
2022-12-06 17:51:56 +0800

    implement about page

---
+++ src/images/AboutPage/TeamMembers.js
@@ -0,0 +1,74 @@
+// Leadership
+import Amanda from '../AboutPage/Leadership/Amanda Yang_General Secretary.jpg'
+import Kate from '../AboutPage/Leadership/Kate Jung_General Secretary.JPG'
+import Lily from '../AboutPage/Leadership/Lily Yip_President.JPG'
+import Michelle from '../AboutPage/Leadership/Michelle Lai_Vice President.JPG'
+
+// Events
+import Chelsea from '../AboutPage/Events team/Chelsea Tsang_Relations & Logistics.jpeg'
+import Eric from '../AboutPage/Events team/Eric So_Relations & Logistics.jpg'
+import Gia from '../AboutPage/Events team/Gia Karamchandani_Relations & Logistics.jpg'
+import Hugo from '../AboutPage/Events team/Hugo Choy_Relations & Logistics.jpeg'
+import Jackson from '../AboutPage/Events team/Jackson Yip_Event Manager.jpg'
+import Kamakshi from '../AboutPage/Events team/Kamakshi Kapoor_Relations & Logistics.jpg'
+import Kathy from '../AboutPage/Events team/Kathy Ng_Relations & Logistics.JPG'
+import Maggie from '../AboutPage/Events team/Maggie Lee_Relations & Logistics.JPG'
+import Papa from '../AboutPage/Events team/Papa Wang_Event Manager.jpg'
+import Yehan from '../AboutPage/Events team/Yehan Ranasinghe_Relations & Logistics.jpg'
+
+// IT
+import Anson from '../AboutPage/IT team/Anson_Chan_IT_Member.jpg'
+import Fortino from '../AboutPage/IT team/Fortino Tan_IT Manager.jpg'
+import Glen from '../AboutPage/IT team/Glen Cheng_IT Member.jpg'
+import Keane from '../AboutPage/IT team/Keane_Hui_IT_Manager.jpg'
+import Sandeepti from '../AboutPage/IT team/Sandeepti Singh_IT Member.jpg'
+
+// Promo
+import Amber from '../AboutPage/Promo team/Amber Wong_Graphic Designer.jpg'
+import Cayenne from '../AboutPage/Promo team/Cayenne Tam_Content Writer.jpg'
+import Erin from '../AboutPage/Promo team/Erin Kang_Promotion Manager.jpeg'
+import Ivy from '../AboutPage/Promo team/Ivy Chan_Graphic Designer.JPG'
+import Jenny from '../AboutPage/Promo team/Jenny Chen_Graphic Designer.JPG'
+import Justin from '../AboutPage/Promo team/Justin Cheng_Promotion Manager.jpeg'
+import Nathan from '../AboutPage/Promo team/Nathan Lau_Content Writer.JPG'
+import Pranav from '../AboutPage/Promo team/Pranav Agarwal_Graphic Designer.jpeg'
+import Rainbow from '../AboutPage/Promo team/Rainbow Zhuang_Video Creator.JPG'
+import Roi from '../AboutPage/Promo team/Roi Cruz_Content Writer.jpg'
+import Sherry from '../AboutPage/Promo team/Sherry Leung_Video Creator.jpg'
+import Wanci from '../AboutPage/Promo team/Wanci Chan_Video Creator.jpg'
+
+const TeamMembers = {
+    Amanda: {imgURL: Amanda, role: 'General Secretary'}, 
+    Kate: {imgURL: Kate, role: 'General Secretary'}, 
+    Lily: {imgURL: Lily, role: 'President'}, 
+    Michelle: {imgURL: Michelle, role: 'Vice President'}, 
+    Chelsea: {imgURL: Chelsea, role: 'Relations & Logistics'},
+    Eric: {imgURL: Eric, role: 'Relations & Logistics'}, 
+    Gia: {imgURL: Gia, role: 'Relations & Logistics'}, 
+    Hugo: {imgURL: Hugo, role: 'Relations & Logistics'}, 
+    Jackson: {imgURL: Jackson, role: 'Event Manager'}, 
+    Kamakshi: {imgURL: Kamakshi, role: 'Relations & Logistics'}, 
+    Kathy: {imgURL: Kathy, role: 'Relations & Logistics'}, 
+    Maggie: {imgURL: Maggie, role: 'Relations & Logistics'}, 
+    Papa: {imgURL: Papa, role: 'Event Manager'}, 
+    Yehan: {imgURL: Yehan, role: 'Relations & Logistics'},
+    Anson: {imgURL: Anson, role: 'IT_Member'}, 
+    Fortino: {imgURL: Fortino, role: 'IT_Manager'}, 
+    Glen: {imgURL: Glen, role: 'IT_Member'}, 
+    Keane: {imgURL: Keane, role: 'IT_Manager'}, 
+    Sandeepti: {imgURL: Sandeepti, role: 'IT_Member'}, 
+    Amber: {imgURL: Amber, role: 'Graphic Designer'},
+    Cayenne: {imgURL: Cayenne, role: 'Content Writer'}, 
+    Erin: {imgURL: Erin, role: 'Promotion Manager'}, 
+    Ivy: {imgURL: Ivy, role: 'Graphic Designer'}, 
+    Jenny: {imgURL: Jenny, role: 'Graphic Designer'}, 
+    Justin: {imgURL: Justin, role: 'Promotion Manager'}, 
+    Nathan: {imgURL: Nathan, role: 'Content Writer'}, 
+    Pranav: {imgURL: Pranav, role: 'Graphic Designer'}, 
+    Rainbow: {imgURL: Rainbow, role: 'Video Creator'}, 
+    Roi: {imgURL: Roi, role: 'Content Writer'}, 
+    Sherry: {imgURL: Sherry, role: 'Video Creator'}, 
+    Wanci: {imgURL: Wanci, role: 'Video Creator'}
+};
+
+export default TeamMembers;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import HeaderAndNavbar from '../components/HeaderAndNavbar';
+import '../styles/About.css'
+
+import TM from '../images/AboutPage/TeamMembers';
 
 class About extends Component {
     state = {  } 
@@ -7,10 +10,123 @@ class About extends Component {
         return (
             <React.Fragment>
                 <HeaderAndNavbar />
-                <h1>ABOUT PAGE</h1>
+                <AboutContent />
+                <div style={{ backgroundColor: 'white', width: '100%', height: '500px' }}></div>
             </React.Fragment>
         );
     }
 }
+
+function AboutContent() {
+    return (
+        <div>
+            <LearnAboutOurTeam />
+            <DidYouKnowThat />
+            <MeetTheTeam />
+        </div>
+    )
+}
+
+function LearnAboutOurTeam() {
+    return (
+        <React.Fragment>
+            <h1 className='about-title'>Learn about our teams! </h1>
+            <h2 className='about-subtitle'>one team, one <span className='about-dream'>dream</span> </h2>
+            <div className='about-description'>
+                <p className='about-description-text'>
+                    We are a student-<br />run community <br />aiming to promote <br />Entrepreneurship <br />Education in <br />HKUST. 
+                    <i class="fa-solid fa-arrow-right" style={{ paddingLeft: '60px' }}></i>
+                </p>
+            </div>
+        </React.Fragment>
+    )
+}
+
+function DidYouKnowThat() {
+    return (
+        <React.Fragment>
+            <div className='about-did-you-know'>Did you know that...</div>
+            <div className='about-did-you-know-1'>The BASE was first founded in <span style={{color: '#F1C718'}}>2015!</span> </div>
+            <div className='about-did-you-know-2'>The BASE is <span style={{color: '#59B4E8'}}>one of the first</span> student led groups that <br />focuses on entrepreneurship and innovation! </div>
+            <div className='about-did-you-know-3'>The BASE has now helped more than 50+ startups. </div>
+        </React.Fragment>
+    )
+}
+
+function MeetTheTeam() {
+    const Color = {
+        blue: '#2B7DC9', 
+        yellow: '#F4C51E', 
+        red: '#B0140A', 
+        green: '#21A797'
+    }
+    return (
+        <React.Fragment>
+            <div className='about-meet-the-team'>Meet the team! </div>
+            <div className='about-profiles' style={{ marginTop: '70px' }}>
+                <Profile name='Lily' backgroundColor={Color.blue} />
+                <Profile name='Michelle' backgroundColor={Color.blue} />
+                <Profile name='Amanda' backgroundColor={Color.blue} />
+                <Profile name='Kate' backgroundColor={Color.blue} />
+            </div>
+            <div className='about-profiles' style={{ marginTop: '81px' }}>
+                <Profile name='Papa' backgroundColor={Color.yellow} />
+                <Profile name='Jackson' backgroundColor={Color.yellow} />
+                <Profile name='Hugo' backgroundColor={Color.yellow} />
+                <Profile name='Kamakshi' backgroundColor={Color.yellow} />
+                <Profile name='Eric' backgroundColor={Color.yellow} />
+            </div>
+            <div className='about-profiles' style={{ marginTop: '40px' }}>
+                <Profile name='Maggie' backgroundColor={Color.yellow} />
+                <Profile name='Gia' backgroundColor={Color.yellow} />
+                <Profile name='Yehan' backgroundColor={Color.yellow} />
+                <Profile name='Kathy' backgroundColor={Color.yellow} />
+                <Profile name='Chelsea' backgroundColor={Color.yellow} />
+            </div>
+            <div className='about-profiles' style={{ marginTop: '81px' }}>
+                <Profile name='Keane' backgroundColor={Color.red} />
+                <Profile name='Fortino' backgroundColor={Color.red} />
+                <Profile name='Sandeepti' backgroundColor={Color.red} />
+                <Profile name='Glen' backgroundColor={Color.red} />
+                <Profile name='Anson' backgroundColor={Color.red} />
+            </div>
+            <div className='about-profiles' style={{ marginTop: '81px' }}>
+                <Profile name='Erin' backgroundColor={Color.green} />
+                <Profile name='Justin' backgroundColor={Color.green} />
+                <Profile name='Nathan' backgroundColor={Color.green} />
+                <Profile name='Cayenne' backgroundColor={Color.green} />
+                <Profile name='Roi' backgroundColor={Color.green} />
+            </div>
+            <div className='about-profiles' style={{ marginTop: '40px' }}>
+                <Profile name='Ivy' backgroundColor={Color.green} />
+                <Profile name='Jenny' backgroundColor={Color.green} />
+                <Profile name='Pranav' backgroundColor={Color.green} />
+                <Profile name='Amber' backgroundColor={Color.green} />
+            </div>
+            <div className='about-profiles' style={{ marginTop: '40px' }}>
+                <Profile name='Wanci' backgroundColor={Color.green} />   
+                <Profile name='Rainbow' backgroundColor={Color.green} />
+                <Profile name='Sherry' backgroundColor={Color.green} />
+            </div>
+            
+        </React.Fragment>
+    )
+}
+
+function Profile(props) {
+    return (
+        <React.Fragment>
+            <div className='about-profile'>
+                <img src={TM[props.name].imgURL} alt="" className='about-profile-img' />
+                <div className='about-profile-description' style={{backgroundColor: props.backgroundColor}}>
+                    {props.name}<br />
+                    {TM[props.name].role}
+                </div>
+            </div>
+        </React.Fragment>
+    )
+}
+
+
  
 export default About;
